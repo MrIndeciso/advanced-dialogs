0ceb63288687054a3955bdf62eb343bc65fe64a1
Roberto <mrindeciso@libero.it>
2021-03-07 16:34:29 +0100

    Sometimes I just hate myself for the things I do wrong

--- advanced-dialogs/src/main/java/com/github/mrindeciso/advanced_dialogs/customDialog/CustomDialogInterface.kt
+++ advanced-dialogs/src/main/java/com/github/mrindeciso/advanced_dialogs/customDialog/CustomDialogInterface.kt
@@ -3,16 +3,15 @@ package com.github.mrindeciso.advanced_dialogs.customDialog
 import android.view.LayoutInflater
 import androidx.viewbinding.ViewBinding
 
-abstract class CustomDialogInterface<out T : ViewBinding> {
+abstract class CustomDialogInterface<out T : ViewBinding>(
+        val autoDismiss: Boolean = true,
+        val makeBackgroundTransparent: Boolean = false,
+) {
 
     abstract fun render(inflater: LayoutInflater): T
 
     fun onDismiss() {}
 
     var dismiss: () -> Unit = {}
-
-    val autoDismiss: Boolean = true
-
-    val makeBackgroundTransparent: Boolean = false
 
 }
